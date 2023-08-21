f157bf461c59fe42eae16fc8e424f97332910529
Esteban Bauer <eabestudio@gmail.com>
2023-08-21 19:15:24 -0300

    Se establecen comisiones directo a Cerebro y se establece el sizer que establece la posición que se utilizará en cada Trade. (AllIn es todo). Falta arreglar que coincidan los valores de Final Portfolio Value con los obtenidos en el excel

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ with pd.ExcelWriter(path) as writer:
     for stock_ticker, stock_dataframe in zip(stocks.names, stocks.dataframes):
         data = bt.feeds.PandasData(dataname=stock_dataframe)
         cerebro = bt.Cerebro()
+        cerebro.broker.set_cash(10000)
+        cerebro.addsizer(bt.sizers.AllInSizerInt)
         cerebro.adddata(data)
 
-        # Establece la cantidad inicial de dinero
-        cerebro.broker.set_cash(10000)
+        # Establece las comisiones y slippage
+        cerebro.broker.set_slippage_perc(0.01)
+        cerebro.broker.setcommission(commission=0.01, commtype=bt.CommInfoBase.COMM_PERC)
+        #cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, name='Trades')
 
-        # Optimización: prueba combinaciones de medias móviles desde 5 hasta 30 para la rápida y desde 40 hasta 100 para la lenta
+        #cerebro.addanalyzer(bt.analyzers.Transactions, name='Transactions')
         cerebro.optstrategy(SMACross, ticker_name = stock_ticker, fast_length=range(5, 7), slow_length=range(10, 12), excel_writer=writer)
-
         cerebro.run(maxcpus=1)  # maxcpus=1 para evitar problemas con multiprocesamiento en algunos entornos
-
